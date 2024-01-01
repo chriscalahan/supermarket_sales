6e9a9ff9b91d1a217da9901ddd68de06fa02a33d
chriscalahan <83083501+chriscalahan@users.noreply.github.com>
2024-01-01 13:17:09 -0500

    Update supplier_total.sql

--- SQL scripts/supplier_total.sql
+++ SQL scripts/supplier_total.sql
@@ -1,18 +1,18 @@
-# Number of products supplied per Supplier with percentage of total over span of data history (4 years)
+# Number of products supplied per Supplier with the percentage of total throughout data history (4 years)
 
-	SELECT supplier,
+    SELECT supplier,
     neighborhood AS delivered_to,
-	COUNT(ws.product_id) total_supplied,
-	(COUNT(ws.product_id)/(SELECT COUNT(*) FROM walmart_sales)) * 100 AS percent
-	FROM walmart_products wp
-	JOIN walmart_sales ws
-		ON wp.product_id = ws.product_id
+    COUNT(ws.product_id) total_supplied,
+    (COUNT(ws.product_id)/(SELECT COUNT(*) FROM walmart_sales)) * 100 AS percent
+    FROM walmart_products wp
+    JOIN walmart_sales ws
+	ON wp.product_id = ws.product_id
     JOIN walmart_inventory wi
-		ON ws.product_id = wi.product_id
-	GROUP BY supplier, neighborhood
-	ORDER BY total_supplied DESC;
+	ON ws.product_id = wi.product_id
+    GROUP BY supplier, neighborhood
+    ORDER BY total_supplied DESC;
     
-	# Check to validate that each store has a single location for above script
+# Check to validate that each store has a single location for the above script
 
     SELECT DISTINCT * FROM
     (SELECT store_name, neighborhood
