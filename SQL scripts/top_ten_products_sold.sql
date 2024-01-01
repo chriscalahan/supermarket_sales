40d55ef336072e61168833345d690774d483e45d
chriscalahan <83083501+chriscalahan@users.noreply.github.com>
2024-01-01 13:20:57 -0500

    Update top_ten_products_sold.sql

--- SQL scripts/top_ten_products_sold.sql
+++ SQL scripts/top_ten_products_sold.sql
@@ -1,9 +1,9 @@
 # Top ten products sold and value over the entire period
 
-SELECT product_name, ROUND(SUM(unit_price * quantity),2) AS total_sales
-FROM walmart_products wp
-JOIN walmart_sales ws
-	ON wp.product_id = ws.product_id
-GROUP BY product_name
-ORDER BY total_sales DESC
-LIMIT 10;
+	SELECT product_name, ROUND(SUM(unit_price * quantity),2) AS total_sales
+	FROM walmart_products wp
+	JOIN walmart_sales ws
+		ON wp.product_id = ws.product_id
+	GROUP BY product_name
+	ORDER BY total_sales DESC
+	LIMIT 10;
