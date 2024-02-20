d3099526f28a2ee23729905ce554d3c6c40511a1
chriscalahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-20 10:32:30 -0500

    Update top_ten_potential_sales.sql

--- SQL scripts/top_ten_potential_sales.sql
+++ SQL scripts/top_ten_potential_sales.sql
@@ -4,11 +4,11 @@
 	(
 		SELECT store_name,
 		ROUND(SUM(quantity_available * unit_price),2) AS potential_sales
-		FROM walmart_inventory wi
-		JOIN walmart_sales ws
-			ON wi.product_id = ws.product_id
-		JOIN walmart_products wp
-			ON ws.product_id = wp.product_id
+		FROM supermarket_inventory si
+		JOIN supermarket_sales ss
+			ON si.product_id = ss.product_id
+		JOIN supermarket_products sp
+			ON ss.product_id = sp.product_id
 		GROUP BY store_name
 		ORDER BY potential_sales DESC
 		LIMIT 10
