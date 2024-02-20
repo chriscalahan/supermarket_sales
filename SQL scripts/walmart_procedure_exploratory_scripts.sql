b701f71e2202424e97cc7c5977281f78ee36e031
chriscalahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-20 10:41:02 -0500

    Update walmart_procedure_exploratory_scripts.sql

--- SQL scripts/walmart_procedure_exploratory_scripts.sql
+++ SQL scripts/walmart_procedure_exploratory_scripts.sql
@@ -2,8 +2,8 @@
 
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS walmart_analysis_scripts;
-CREATE PROCEDURE walmart_analysis_scripts ()
+DROP PROCEDURE IF EXISTS supermarket_analysis_scripts;
+CREATE PROCEDURE supermarket_analysis_scripts ()
 BEGIN
 
 	# TOP 10 stores in potential sales based on inventory
@@ -12,11 +12,11 @@ BEGIN
 	(
 		SELECT store_name,
 		ROUND(SUM(quantity_available * unit_price),2) AS potential_sales
-		FROM walmart_inventory wi
-		JOIN walmart_sales ws
-			ON wi.product_id = ws.product_id
-		JOIN walmart_products wp
-			ON ws.product_id = wp.product_id
+		FROM supermarket_inventory si
+		JOIN supermarket_sales ss
+			ON si.product_id = ss.product_id
+		JOIN supermarket_products sp
+			ON ss.product_id = sp.product_id
 		GROUP BY store_name
 		ORDER BY potential_sales DESC
 		LIMIT 10
@@ -35,9 +35,9 @@ BEGIN
 		ROUND(SUM(unit_price * quantity),2) AS total_sales, 
 		YEAR(date) AS sale_year,
 		DENSE_RANK() OVER(PARTITION BY store_name ORDER BY ROUND(SUM(unit_price * quantity),2) DESC) ranking
-		FROM walmart_sales ws
-		JOIN walmart_inventory wi
-			ON ws.product_id = wi.product_id
+		FROM supermarket_sales ss
+		JOIN supermarket_inventory si
+			ON ss.product_id = si.product_id
 		GROUP BY sale_year, store_name
 		ORDER BY ranking, sale_year
         )
@@ -51,9 +51,9 @@ BEGIN
 	# TOP 10 records in actual sales over the last 4 years; Grouped by store and year
 
 	SELECT store_name, ROUND(SUM(unit_price * quantity),2) AS total_sales, YEAR(date) AS year
-	FROM walmart_sales ws
-	JOIN walmart_inventory wi
-		ON ws.product_id = wi.product_id
+	FROM supermarket_sales ss
+	JOIN supermarket_inventory si
+		ON ss.product_id = si.product_id
 	GROUP BY store_name, year
 	ORDER BY total_sales DESC, store_name, year
 	LIMIT 10;
@@ -63,13 +63,13 @@ BEGIN
 
 	SELECT supplier,
    	neighborhood AS delivered_to,
-	COUNT(ws.product_id) total_supplied,
-	(COUNT(ws.product_id)/(SELECT COUNT(*) FROM walmart_sales)) * 100 AS percent
-	FROM walmart_products wp
-	JOIN walmart_sales ws
-		ON wp.product_id = ws.product_id
-    	JOIN walmart_inventory wi
-		ON ws.product_id = wi.product_id
+	COUNT(ss.product_id) total_supplied,
+	(COUNT(ss.product_id)/(SELECT COUNT(*) FROM supermarket_sales)) * 100 AS percent
+	FROM supermarket_products sp
+	JOIN supermarket_sales ss
+		ON sp.product_id = ss.product_id
+    	JOIN supermarket_inventory si
+		ON ss.product_id = si.product_id
 	GROUP BY supplier, neighborhood
 	ORDER BY total_supplied DESC;
     
@@ -79,24 +79,24 @@ BEGIN
 	SELECT DISTINCT * 
 	FROM
   	(SELECT store_name, neighborhood
-   	FROM walmart_inventory
+   	FROM supermarket_inventory
    	ORDER BY store_name) validate;
 
 	# Total sales over 4 years by geographical area
 
 	SELECT neighborhood, ROUND(SUM(unit_price * quantity),2) AS total_sales
-	FROM walmart_sales ws
-	JOIN walmart_inventory wi
-		ON ws.product_id = wi.product_id
+	FROM supermarket_sales ss
+	JOIN supermarket_inventory si
+		ON ss.product_id = si.product_id
 	GROUP BY neighborhood
 	ORDER BY total_sales DESC;
 
 	# Top ten products sold and value over the entire period
 
 	SELECT product_name, ROUND(SUM(unit_price * quantity),2) AS total_sales
-	FROM walmart_products wp
-	JOIN walmart_sales ws
-		ON wp.product_id = ws.product_id
+	FROM supermarket_products sp
+	JOIN supermarket_sales ss
+		ON sp.product_id = ss.product_id
 	GROUP BY product_name
 	ORDER BY total_sales DESC
 	LIMIT 10;
