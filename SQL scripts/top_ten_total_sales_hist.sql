e29c666a0bebe3443d302109fb354fa7488ddbbc
chriscalahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-20 10:33:56 -0500

    Update top_ten_total_sales_hist.sql

--- SQL scripts/top_ten_total_sales_hist.sql
+++ SQL scripts/top_ten_total_sales_hist.sql
@@ -2,9 +2,9 @@
 # TOP 10 records in actual sales over the last 4 years; Grouped by store and year
 
 	SELECT store_name, ROUND(SUM(unit_price * quantity),2) AS total_sales, YEAR(date) AS year
-	FROM walmart_sales ws
-	JOIN walmart_inventory wi
-		ON ws.product_id = wi.product_id
+	FROM supermarket_sales ss
+	JOIN supermarket_inventory si
+		ON ss.product_id = si.product_id
 	GROUP BY store_name, year
 	ORDER BY total_sales DESC, store_name, year
 	LIMIT 10;
