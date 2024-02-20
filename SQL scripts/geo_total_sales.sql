992236c24568070d10609c929a42e894025c64b4
chriscalahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-20 10:20:34 -0500

    Update geo_total_sales.sql

--- SQL scripts/geo_total_sales.sql
+++ SQL scripts/geo_total_sales.sql
@@ -1,8 +1,8 @@
 # Total sales over 4 years by geographical area
 
 	SELECT neighborhood, ROUND(SUM(unit_price * quantity),2) AS total_sales
-	FROM walmart_sales ws
-	JOIN walmart_inventory wi
-		ON ws.product_id = wi.product_id
+	FROM supermarket_sales ss
+	JOIN supermarket_inventory si
+		ON ss.product_id = si.product_id
 	GROUP BY neighborhood
 	ORDER BY total_sales DESC;
