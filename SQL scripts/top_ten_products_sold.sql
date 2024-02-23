95866be9bcfa98abdbdfb0b9e69de23cfa7af518
Chris Calahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-23 13:22:11 -0500

    Update top_ten_products_sold.sql

--- SQL scripts/top_ten_products_sold.sql
+++ SQL scripts/top_ten_products_sold.sql
@@ -1,4 +1,4 @@
-# Top ten products sold and value over the entire period
+# Top ten products sold by total value over the entire period
 
 	SELECT product_name, ROUND(SUM(unit_price * quantity),2) AS total_sales
 	FROM supermarket_products sp
