8e061e8325e17f1bca1432010c0a8a675f4bf83c
Chris Calahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-23 13:26:48 -0500

    Update product_detail_metrics.sql

--- SQL scripts/product_detail_metrics.sql
+++ SQL scripts/product_detail_metrics.sql
@@ -1,15 +1,15 @@
 # Product Details - Metrics by product, store, and location
 
-SELECT product_name, supplier, neighborhood, 
-SUM(quantity) AS total_quantity, 
-ROUND(AVG(unit_price),2) AS avg_price, 
-ROUND(SUM(unit_price * quantity),2) AS total_sales
-FROM walmart_products sp
-JOIN walmart_sales ss
-	ON sp.product_id = ss.product_id
-JOIN walmart_inventory si
-	ON ss.product_id = si.product_id
-GROUP BY product_name, supplier, neighborhood
-ORDER BY total_sales DESC, total_quantity DESC;
+	SELECT product_name, supplier, neighborhood, 
+		SUM(quantity) AS total_quantity, 
+		ROUND(AVG(unit_price),2) AS avg_price, 
+		ROUND(SUM(unit_price * quantity),2) AS total_sales
+	FROM supermarket_products sp
+	JOIN supermarket_sales ss
+		ON sp.product_id = ss.product_id
+	JOIN supermarket_inventory si
+		ON ss.product_id = si.product_id
+	GROUP BY product_name, supplier, neighborhood
+	ORDER BY total_sales DESC, total_quantity DESC;
 
-    
+# It's important to view each metric together as the total sales don't necessarily mean it's a better-performing item. Unit cost and quantity ordered will play a role.
