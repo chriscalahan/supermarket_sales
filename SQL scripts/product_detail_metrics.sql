1b9fabd49a634332349cefb9030e4f92cc90d0a8
Chris Calahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-23 13:27:42 -0500

    Update product_detail_metrics.sql

--- SQL scripts/product_detail_metrics.sql
+++ SQL scripts/product_detail_metrics.sql
@@ -1,5 +1,7 @@
 # Product Details - Metrics by product, store, and location
-
+# It's important to view each metric together as the total sales don't necessarily mean it's a better-performing item. Unit cost and quantity ordered will play a 
+# role.
+	
 	SELECT product_name, supplier, neighborhood, 
 		SUM(quantity) AS total_quantity, 
 		ROUND(AVG(unit_price),2) AS avg_price, 
@@ -12,4 +14,3 @@
 	GROUP BY product_name, supplier, neighborhood
 	ORDER BY total_sales DESC, total_quantity DESC;
 
-# It's important to view each metric together as the total sales don't necessarily mean it's a better-performing item. Unit cost and quantity ordered will play a role.
