78395cdbfd80c2c0c33705ff5d5dae8af43c7b11
Chris Calahan <83083501+chriscalahan@users.noreply.github.com>
2024-02-24 13:12:36 -0500

    Add files via upload

---
+++ SQL scripts/stocked_prod_quantity_corr.sql
@@ -0,0 +1,40 @@
+# Create a temporary table showing the top ten products sold by the 3 best stores
+
+CREATE temporary table temp_table AS 
+	SELECT product_name,
+			SUM(quantity_available) AS quantity_avilable,
+			SUM(quantity) AS total_quantity, 
+			ROUND(AVG(unit_price),2) AS avg_price, 
+			ROUND(SUM(unit_price * quantity),2) AS total_sales
+	FROM walmart_products sp
+	JOIN walmart_sales ss
+		ON sp.product_id = ss.product_id
+	JOIN walmart_inventory si
+		ON ss.product_id = si.product_id
+	WHERE supplier IN ('Ben Franklin','Shopko','Family Dollar')
+	GROUP BY product_name, supplier, neighborhood
+	ORDER BY total_sales DESC, total_quantity DESC
+	LIMIT 10;
+# Create a secondary temp table containing a single column or product names from the primary temp table
+
+CREATE temporary table t2 AS
+	SELECT product_name 
+	FROM temp_table;
+
+# Querying all products and relevant metrics from all stores compared to 3 best stores
+# The lower the ranking (1-4), the less inventory they stocked for that product
+
+SELECT product_name,
+		SUM(quantity_available) AS quantity_available,
+		SUM(quantity) AS total_quantity, 
+		ROUND(AVG(unit_price),2) AS avg_price, 
+		ROUND(SUM(unit_price * quantity),2) AS total_sales,
+        DENSE_RANK() OVER(PARTITION BY product_name ORDER BY SUM(quantity_available) DESC) AS ranking
+FROM walmart_products sp
+JOIN walmart_sales ss
+	ON sp.product_id = ss.product_id
+JOIN walmart_inventory si
+	ON ss.product_id = si.product_id
+WHERE product_name IN (SELECT * FROM t2)
+GROUP BY product_name, supplier, neighborhood
+ORDER BY total_sales DESC, total_quantity DESC;
